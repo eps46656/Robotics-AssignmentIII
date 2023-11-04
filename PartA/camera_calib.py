3019b44393c5e73780becf89fe84dab661fb9147
eps46656 <eps46656@gmail.com>
2023-11-04 22:14:05 +0800

    0

--- PartA/camera_calib.py
+++ PartA/camera_calib.py
@@ -1,12 +1,16 @@
 
 import cv2 as cv
 import numpy as np
+import os
+import random
+
+DIR = os.path.dirname(__file__).replace("\\", "/")
 
 def CameraClib(imgs):
-    pattern_h = 11
-    pattern_w = 11
+    pattern_h = 8
+    pattern_w = 6
 
-    per_objpoints = np.zeros([pattern_h * pattern_w, 3])
+    per_objpoints = np.zeros([pattern_h * pattern_w, 3], dtype=np.float32)
     per_objpoints[:, :2] = np.mgrid[:pattern_h, :pattern_w] \
                            .transpose().reshape([-1, 2])
 
@@ -17,9 +21,9 @@ def CameraClib(imgs):
         gray = cv.cvtColor(img, cv.COLOR_RGB2GRAY)
 
         ret, corners = cv.findChessboardCorners(
-            gray, [pattern_h, pattern_w], None)
+            gray, (pattern_h, pattern_w), None)
 
-        if ret == False:
+        if not ret:
             continue
 
         objpoints.append(per_objpoints)
@@ -29,27 +33,43 @@ def CameraClib(imgs):
 
         imgpoints.append(corners)
 
-        uimg = cv.UMat(img)
-        cv.drawChessboardCorners(uimg, [pattern_h, pattern_w], corners, True)
-        cv.imshow(uimg)
-        cv.waitKey(45)
+        # uimg = cv.UMat(img)
+        # cv.drawChessboardCorners(uimg, [pattern_h, pattern_w], corners, True)
+        # cv.imshow("uimg", uimg)
+        # cv.waitKey(45)
 
-    cv.destryoAllWindows()
-
-    print(f"gray.shape = {gray.shape}")
+    # cv.destroyAllWindows()
 
     ret, camera_mat, camera_distort, rvecs, tvecs = cv.calibrateCamera(
-        objpoints, imgpoints, gray.shape[:-1], None, None)
+        objpoints, imgpoints, gray.shape[::-1], None, None)
+
+    return camera_mat, camera_distort
 
 def main():
     imgs = list()
 
     # for i in range()
 
-    CameraClib([])
+    num_of_frames = 1211
 
+    frame_idxes = sorted(random.choices(range(num_of_frames), k=64))
 
-    # cv.findChessboardCorners(gray )
+    print(f"frame_idxes = {frame_idxes}")
+
+    for frame_i in frame_idxes:
+        img = cv.imread(f"{DIR}/vedio_frames/frame-{frame_i}.png")
+
+        imgs.append(img)
+
+    camera_mat, camera_distort = CameraClib(imgs)
+
+    print("camera_mat = ")
+    print(camera_mat)
+    print("camera_distort = ")
+    print(camera_distort)
+
+    np.save(f"{DIR}/camera_params.npy", {"camera_mat": camera_mat,
+                                         "camera_distort": camera_distort})
 
 if __name__ == "__main__":
     main()
